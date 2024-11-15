4e45658bc3243f28b3ea8fda31df10c4b0be7eea
Manish Pundora <manish.pundora@ericsson.com>
2024-11-15 19:43:14 +0530

    Folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Gen AI Medical Chatbot',
+    version='0.0.0',
+    author='Manish Pundora',
+    author_email='manishpundora@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
